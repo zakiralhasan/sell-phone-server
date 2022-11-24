16ab8c75ffecb16e7abfa5de62ee0968d03794c2
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-25 03:39:14 +0600

    user data stored through user collection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const { MongoClient, ServerApiVersion } = require('mongodb'); //used for mongDB
 const port = process.env.PORT || 5000;
 
 //used for .env file 
@@ -12,14 +12,29 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.krkb3gw.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.krkb3gw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const usersCollection = client.db('sellPhone').collection('users')
+
+        // Save user information to the user collection
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body
+            console.log(userInfo)
+            const result = await usersCollection.insertOne(userInfo)
+            console.log(result)
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+
+//called run() function, which working for store and get data from mongoDB
+run().catch(error => console.log(error));
 
 
 app.get("/", (req, res) => {
