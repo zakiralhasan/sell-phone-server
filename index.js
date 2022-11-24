2e8b7c44e56cb0573fcedabc25b4853a2d014a03
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-24 17:18:29 +0600

    mongodb installed

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 
+//used for .env file 
+require("dotenv").config();
+
 // midleware
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.krkb3gw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 
 app.get("/", (req, res) => {
