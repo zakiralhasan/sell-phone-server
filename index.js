04a2e0ee6fbb3507df4820fbee40310e764e3a25
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-25 15:08:46 +0600

    jwt installed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb'); //used for mongDB
+const jwt = require('jsonwebtoken');// used for jwt token
 const port = process.env.PORT || 5000;
 
 //used for .env file 
@@ -20,12 +21,20 @@ async function run() {
         const usersCollection = client.db('sellPhone').collection('users')
 
         // Save user information to the user collection
-        app.post('/users', async (req, res) => {
+        app.put('/users', async (req, res) => {
             const userInfo = req.body
             console.log(userInfo)
-            const result = await usersCollection.insertOne(userInfo)
+            const email = req.body.email
+            console.log(email)
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: userInfo
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options)
             console.log(result)
-            res.send(result)
+            const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN, { expiresIn: '1d' });
+            res.send({ result, token })
         })
     }
     finally {
